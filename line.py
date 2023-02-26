b03ee41dff5c9b3ea50ba7e49d50853fd0e75dd5
AZIZBEKRAXMONBERDIYEV <jxechkim079@gmail.com>
2023-02-27 00:15:46 +0500

    line.py

--- line.py
+++ line.py
@@ -1,3 +1,4 @@
+from math import pow
 class Line:
     def __init__(self, x1, y1, x2, y2) -> None:
         self.x1 = x1
@@ -14,4 +15,4 @@ class Line:
         Returns:
             float or int: distance.
         """
-        pass
+        return pow((self.x1-self.x2)**1/2 + (self.y1-self.y2)**1/2)
