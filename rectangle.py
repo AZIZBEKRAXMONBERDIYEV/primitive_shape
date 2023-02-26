8e256d18194b733ba8d567866c44aad5d901c2d6
AZIZBEKRAXMONBERDIYEV <jxechkim079@gmail.com>
2023-02-27 00:16:51 +0500

    rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,4 +1,15 @@
 from polygon import Polygon
 
 class Rectangle(Polygon):
-    pass
+    def __init__(self, eni, buyi): 
+        super().__init__(eni, buyi ) 
+        self.eni=eni 
+        self.buyi=buyi 
+
+
+    def yusi(self):
+        return self.eni * self.buyi
+    
+
+    def piremetir(self):
+        return 2*(self.eni +self.buyi)
