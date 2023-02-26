a35d6303b2551604be752212887a56abe71edb26
AZIZBEKRAXMONBERDIYEV <jxechkim079@gmail.com>
2023-02-27 00:17:19 +0500

    square.py

--- square.py
+++ square.py
@@ -1,4 +1,12 @@
 from polygon import Polygon
 
 class Square(Polygon):
-    pass
+    def __init__(self, uzunlik): 
+        super().__init__(uzunlik ) 
+        self.uzunlik=uzunlik
+
+    def yusi(selt):
+        return selt.uzunlik**2
+    
+    def premetir(self):
+        return 4*self.uzunlik
